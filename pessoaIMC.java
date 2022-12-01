7ff0a83ac6e53e8cc9ac1834b1e29d6e5518ee1f
Maria Eduarda Pena <111512404+DudaPenaa@users.noreply.github.com>
2022-12-01 08:50:50 -0300

    Add files via upload

---
+++ pessoaIMC.java
@@ -0,0 +1,31 @@
+package atividade;
+
+public abstract class PessoaIMC extends Pessoa {
+
+    public PessoaIMC(String nome, String dataNascimento, double peso, double altura) {
+        super(nome, dataNascimento);
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    private double peso;
+    private double altura;
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double calcularIMC(double peso, double altura) {
+        return peso / (altura * altura);
+    }
+
+    public String toString() {
+        return super.toString() + ", Peso: " + this.peso + ", Altura: " + this.altura;
+    }
+
+    public abstract String resultIMC();
+}
